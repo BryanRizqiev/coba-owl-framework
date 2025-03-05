213dd69801aea5a57609b6ca565db8b8a04b2ded
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2025-03-05 15:31:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { Component, mount, xml } = owl;
 
 // Owl Components
 class Root extends Component {
-  static template = xml`<div>Hello World</div>`;
+  static template = xml`<div>Hello World, Bryan</div>`;
 }
 
 mount(Root, document.body);
